47fb4f7f19242fc8e0849c600834f0395063b570
mukisa <emukisa782@gmail.com>
2026-09-19 21:33:33 +0300

    fix(render): synchronize pnpm-lock.yaml and update render buildCommand

--- api/index.js
+++ api/index.js
@@ -56559,7 +56559,7 @@ function date5(params) {
 // ../../node_modules/.pnpm/zod@3.25.76/node_modules/zod/v4/classic/external.js
 config(en_default2());
 
-// ../../node_modules/.pnpm/drizzle-zod@0.8.3_drizzle-o_9023862330606cbd2d7b4dbe0f6a6add/node_modules/drizzle-zod/index.mjs
+// ../../node_modules/.pnpm/drizzle-zod@0.8.3_drizzle-orm@0.45.2_@types+pg@8.20.0_pg@8.22.0__zod@3.25.76/node_modules/drizzle-zod/index.mjs
 var CONSTANTS = {
   INT8_MIN: -128,
   INT8_MAX: 127,
